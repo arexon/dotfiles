9ca5cf4a1056b29c9968998602cd658e3820ae31
arexon <arexonreal@gmail.com>
2023-04-17 07:44:16 +0200

    fix(fish): remove PNPM and add `hostname` variable

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,10 @@
 if status --is-interactive
     keychain --quiet --nogui ~/.ssh/id_ed25519
 end
-
-source ~/.keychain/fish
+source ~/.keychain/{{hostname}}-fish
 
 export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME.deno/bin:$PATH"
-export PATH "$HOME/.local/share/pnpm"
 
 export BAT_THEME="catppuccin"
 export EDITOR="nvim"
