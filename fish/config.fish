2e65eae3cd5ddfb4f07037575933a7f6e42266c7
arexon <arexonreal@gmail.com>
2023-09-19 12:00:59 +0300

    chore(fish): remove fly cli and change `$EDITOR` to `hx`

--- fish/config.fish
+++ fish/config.fish
@@ -7,14 +7,12 @@ export PATH="$HOME/.cargo/bin:$PATH"
 export PATH="$HOME/.deno/bin:$PATH"
 export PATH="$HOME/.proto/bin:$PATH"
 fish_add_path ~/.proto/tools/node/globals/bin
-fish_add_path ~/.fly/bin
 
 export BAT_THEME="catppuccin"
-export EDITOR="nvim"
-export VISUAL="nvim"
+export EDITOR="hx"
+export VISUAL="hx"
 
-alias v="nvim"
-alias ls="exa"
-alias bat="batcat"
+alias ls exa
+alias bat batcat
 
 starship init fish | source
