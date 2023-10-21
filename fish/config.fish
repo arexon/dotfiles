34e203e1cd94a07eacb08526e18a7928c1505f89
arexon <arexonreal@gmail.com>
2023-10-21 17:37:46 +0300

    feat(fish): force the shell prompt to be at the bottom

--- fish/config.fish
+++ fish/config.fish
@@ -5,8 +5,10 @@ alias hx helix
 alias zj zellij
 alias ls exa
 alias ll "exa -la"
+alias clear "clear && printf '\e[H\ec\e[100B'"
 
 set -x EDITOR helix
 set -g fish_greeting
 
 starship init fish | source
+printf "\e[H\ec\e[100B"
